d8ef97ecb03d2fed8cdfcbc3652f435fca6a4ab9
ahmad_muzaki <ahmad_muzaki@telkomsel.co.id>
2025-03-30 09:44:47 +0700

    [zaky] - fix url origin cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://server-driven-ui-frontend.vercel.app/', 'https://uyxwtbrsadwxcbmmafli.supabase.co'],
+    origin: ['https://server-driven-ui-frontend.vercel.app', 'http://localhost:3000', 'https://uyxwtbrsadwxcbmmafli.supabase.co'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
